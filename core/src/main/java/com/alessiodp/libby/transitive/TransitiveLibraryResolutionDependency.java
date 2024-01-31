f8230856fe6485ba8945d85034af27a43dde129b
Ghost_chu <ghostchu@111.com>
2024-01-31 23:05:34 +0800

    Revert "WTF?"
    This reverts commit 166d3b20f2487bf764376eca22e3486cb77edf6e.

--- core/src/main/java/com/alessiodp/libby/transitive/TransitiveLibraryResolutionDependency.java
+++ core/src/main/java/com/alessiodp/libby/transitive/TransitiveLibraryResolutionDependency.java
@@ -53,9 +53,9 @@ enum TransitiveLibraryResolutionDependency {
                 .version(version)
                 .checksum(checksum)
                 // Relocate all packages used in Libby to avoid conflicts
-                .relocate("org{}eclipse{}aether{}util", "com.alessiodp.libby.shade.org.eclipse.aether.util") // maven-resolver-util
-                .relocate("org{}eclipse{}aether", "com.alessiodp.libby.shade.org.eclipse.aether") // maven-resolver-api
-                .relocate("org{}apache{}maven{}repository{}internal", "com.alessiodp.libby.shade.org.apache.maven.repository.internal") // maven-resolver-provider
+                .relocate("org{}eclipse{}aether{}util", "org.eclipse.aether.util") // maven-resolver-util
+                .relocate("org{}eclipse{}aether", "org.eclipse.aether") // maven-resolver-api
+                .relocate("org{}apache{}maven{}repository{}internal", "org.apache.maven.repository.internal") // maven-resolver-provider
 
                 .build();
     }
